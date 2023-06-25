e8eb2ad5024016176de8f305c3ea87e80273f7f9
zwirekPL <wirigiliusz@gmail.com>
2023-06-25 15:32:22 +0200

    .

--- back/src/messages/messages.service.js
+++ back/src/messages/messages.service.js
@@ -1,7 +1,4 @@
 const Item = require("../models/item");
-const Storage = require("../models/storage");
-const ShoppingList = require("../models/shopping-list");
-const ShoppingItem = require("../models/shopping-list-item");
 
 const getUserItems = (userName) => {
   const dataFind = Item.find({
@@ -10,27 +7,6 @@ const getUserItems = (userName) => {
   return dataFind;
 };
 
-const getInventoryHistory = (userName) => {
-  const dataFind = Storage.find({
-    userName: `${userName}`,
-  }).exec();
-  return dataFind;
-};
-
-const getShoppingListHistory = (userName) => {
-  const dataFind = ShoppingList.find({
-    userName: `${userName}`,
-  }).exec();
-  return dataFind;
-};
-
-const getShoppingListDrivers = (userName) => {
-  const dataFind = ShoppingItem.find().exec();
-  return dataFind;
-};
 module.exports = {
-  getInventoryHistory,
-  getShoppingListHistory,
   getUserItems,
-  getShoppingListDrivers,
 };
