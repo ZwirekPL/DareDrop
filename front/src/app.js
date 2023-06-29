305ac844bd73b62902a817d0256900dd3d221a22
zwirekPL <wirigiliusz@gmail.com>
2023-06-29 19:27:15 +0200

    .

--- front/src/app.js
+++ front/src/app.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { MainPage } from "./pages/main-page";
+import { StreamerPage } from "./pages/streamerPage";
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/streamer" element={<StreamerPage />} />
     </Routes>
   );
 };
